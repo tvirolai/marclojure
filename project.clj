61701cd73fbe1637d1f23aba9b17b185d7dbe6fa
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2017-03-23 14:18:15 +0200

    freelib-marc4j -> marc4j

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject marc-clojure "0.2.1"
+(defproject marc-clojure "0.2.2"
   :description "A library for parsing MARC records"
   :url "http://github.com/tvirolai/marc-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [info.freelibrary/freelib-marc4j "2.6.12"]])
+                 [org.marc4j/marc4j "2.8.0"]])
