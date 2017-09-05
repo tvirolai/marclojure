f34403a8fd4f7d029e9c11c342a7882e3a4be7e4
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2017-09-05 17:30:21 +0300

    Remove test code from user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -4,12 +4,3 @@
             [marclojure.writer :as writer]))
 
 ;; A testbed namespace
-
-(def file
-  "/home/tvirolai/Melinda-dumppi/dumppi.seq")
-
-(defn calculate []
-  (->> file (parser/load-data :aleph) count))
-
-(def tietue
-  (->> file (parser/load-data :aleph) second))
