f703962b04862956ab92a7f38461c8c80baad448
Tuomo Virolainen <tuomo.virolainen@siili.com>
2021-10-04 08:20:40 +0300

    Remove unnecessary dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marclojure "1.0.6"
+(defproject marclojure "1.0.6-SNAPSHOT"
   :description "A library for parsing MARC records"
   :url "http://github.com/tvirolai/marclojure"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "LATEST"]
                  [org.marc4j/marc4j "2.9.2"]
                  [org.clojure/data.xml "0.0.8"]]
-  :plugins [[lein-cloverage "1.0.11-SNAPSHOT"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :creds :gpg
                                     :sign-releases false}]]
