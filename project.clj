ec24f41313718f21ac36024038e286abee5efbcf
Tuomo Virolainen <tuomo.virolainen@siili.com>
2021-10-04 08:01:54 +0300

    Bump deps

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject marclojure "1.0.5"
+(defproject marclojure "1.0.6"
   :description "A library for parsing MARC records"
   :url "http://github.com/tvirolai/marclojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.marc4j/marc4j "2.8.2"]
+  :dependencies [[org.clojure/clojure "LATEST"]
+                 [org.marc4j/marc4j "2.9.2"]
                  [org.clojure/data.xml "0.0.8"]]
+  :plugins [[lein-cloverage "1.0.11-SNAPSHOT"]]
   :profiles {:dev {:source-paths ["dev"]}})
